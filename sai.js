01b1e2141b321ce60271caee60c3330d5a9d9174
kondurisaivarshath <saivarshathkonduri2@gmail.com>
2025-07-22 14:14:20 +0530

    main commit

---
+++ sai.js
@@ -0,0 +1,12 @@
+import Helloworld from "./app"
+import react from react
+
+function Sai(){
+    render(
+        <div>
+            <h1>Hello World</h1>
+            <Helloworld />
+        </div>
+    )
+}
+export default Sai
